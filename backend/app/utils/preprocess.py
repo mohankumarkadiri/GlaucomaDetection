4b2456bc420eab11d7387a0aeff544ece4988ae2
Kadiri Mohan Kumar <96787015+mohankumarkadiri@users.noreply.github.com>
2025-04-24 19:54:26 +0530

    Update preprocess.py

--- backend/app/utils/preprocess.py
+++ backend/app/utils/preprocess.py
@@ -4,11 +4,10 @@ import numpy as np
 
 def preprocess_image(img_bytes):
     try:
-        img = Image.open(io.BytesIO(img_bytes))
+        img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
         img = img.resize((256, 256))
-        img_array = np.array(img)
+        img_array = np.array(img) / 255.0
         img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array / 255.0
         return img_array, None
     except Exception as e:
         return None, e
